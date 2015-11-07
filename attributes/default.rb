bde352c1a873b056c24366fd2aa92277e8e025f5
chef <chef@ChefWs.takara.org>
2015-11-07 21:57:34 +0900

    first commit

---
+++ attributes/default.rb
@@ -0,0 +1,5 @@
+default["vip"]["address"] = "161.202.132.84"
+default["vip"]["netmask"] = "255.255.255.0"
+default["vip"]["portno"] = "80"
+default["real_servers"] = {"161.202.142.203" => "80", "161.202.142.206" => "80", "161.202.142.199" => "80"}
+default["keepalived"]["state"] = "MASTER"
